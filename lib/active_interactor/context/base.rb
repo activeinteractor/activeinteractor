891bfab0104f618a4acf8a16c909085a572ddf82
Aaron Allen <hello@aaronmallen.me>
2023-10-12 13:43:14 -0500

    fixes a bug with `ActiveInteractor::HasActiveModelErrors`
    - fixed `NoMethodError` `human_attribute_name` for `ActiveInteractor::Context::Base` and `ActiveInteractor::Result`
    - fixed `NoMethodError` `lookup_ancestors` for `ActiveInteractor::Context::Base` and `ActiveInteractor::Result`

--- lib/active_interactor/context/base.rb
+++ lib/active_interactor/context/base.rb
@@ -9,15 +9,9 @@ module ActiveInteractor
       include AttributeAssignment
       include Type::HasTypes
 
-      attr_reader :errors
-
       def initialize(attributes = {})
+        super
         @errors = ActiveModel::Errors.new(self)
-        attribute_set.attributes.each do |attribute|
-          next unless attributes.with_indifferent_access.key?(attribute.name)
-
-          assign_attribute_value(attribute.name, attributes.with_indifferent_access[attribute.name])
-        end
       end
 
       def validate!

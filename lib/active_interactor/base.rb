b05bdba9a3be774efc2f85d058987f5345fd4a85
Aaron Allen <hello@aaronmallen.me>
2023-10-28 14:10:08 -0500

    Prepare the gem for the 2.0.0-alpha.3.0.0 release

--- lib/active_interactor/base.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module ActiveInteractor
-  # The Main interface for ActiveInteractor
-  #
-  # @deprecated will be removed in version 2.0.0-alpha.3.0.0
-  #   use {ActiveInteractor::Interactor::Base} instead
-  class Base < ActiveInteractor::Interactor::Base; end
-end
